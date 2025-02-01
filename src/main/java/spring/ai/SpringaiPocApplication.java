75f667e675b832e4d1fa638d403106940e37e6d9
bantunes82 <bantunes82@gmail.com>
2025-02-01 18:24:55 +0100

    Spring Project template created

---
+++ src/main/java/spring/ai/SpringaiPocApplication.java
@@ -0,0 +1,13 @@
+package spring.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiPocApplication.class, args);
+	}
+
+}
